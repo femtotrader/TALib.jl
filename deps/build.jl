8998cb8790cab94d953329578a235a0e62c38bea
femtotrader <femto.trader@gmail.com>
2016-10-08 22:26:13 +0200

    src/ta-lib for unpacked_dir

--- deps/build.jl
+++ deps/build.jl
@@ -11,13 +11,13 @@ if is_apple()
         error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
     end
     using Homebrew
-    provides(Homebrew.HB, "ta-lib", libta_lib, os = :Darwin )
+    provides(Homebrew.HB, "ta-lib", libta_lib, os = :Darwin)
 
     
 elseif is_linux()
     #const libta_lib = "/usr/lib/libta_lib.so.0.0.0"
     
-    provides(Sources, URI("http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"), libta_lib, unpacked_dir="ta-lib-0.4.0-src")
+    provides(Sources, URI("http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"), libta_lib, unpacked_dir="src/ta-lib")
     provides(BuildProcess, Autotools(libtarget = "libta_lib", configure_options=["--prefix=/usr"]), libta_lib, os = :Unix)
     
 else
